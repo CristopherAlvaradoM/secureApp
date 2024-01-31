5146e4ce1e7f719e0296e03ca6a42b79f74f4b98
Anthony Martinez Arellano <116910131+AnthonyMA5@users.noreply.github.com>
2024-01-31 13:11:30 -0600

    colores e imágenes

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import {createBrowserRouter, RouterProvider} from 'react-router-dom'
 import './index.css'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Home from "./View/home.tsx";
 
 const router = createBrowserRouter([{
-  path: '/',
-  element: <App />,
-}])
+    path: '/',
+    element: <App/>,
+},
+    {
+        path:'/home',
+        element: <Home/>,
+    }])
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>,
+    <React.StrictMode>
+        <RouterProvider router={router}/>
+    </React.StrictMode>,
 )
