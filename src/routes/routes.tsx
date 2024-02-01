5ab3b0b3c130c50ebf5843d3860bfd4bf9cb0116
Anthony Martinez Arellano <116910131+AnthonyMA5@users.noreply.github.com>
2024-01-31 18:24:31 -0600

    add otp_verf route

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,6 +1,7 @@
 import App from "../App"
 import Home from "../View/home"
 import Login from "../View/Login"
+import OTPVerification from "../View/otp_verification.tsx";
 
 export const routes = [
   {
@@ -15,4 +16,8 @@ export const routes = [
     path: "/login",
     element: <Login />,
   },
+  {
+    path: "/otp_verf",
+    element: <OTPVerification />
+  }
 ]
